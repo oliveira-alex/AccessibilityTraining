10d0df35a248fb9213edaf3ca5b0345b069d8a2b
ALeX <alexdeoliveira.poli@gmail.com>
2021-10-24 08:43:03 -0300

    Initial Commit

---
+++ AccessibilityTraining/AccessibilityTrainingApp.swift
@@ -0,0 +1,17 @@
+//
+//  AccessibilityTrainingApp.swift
+//  AccessibilityTraining
+//
+//  Created by Alex Oliveira on 24/10/21.
+//
+
+import SwiftUI
+
+@main
+struct AccessibilityTrainingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ AccessibilityTraining/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  AccessibilityTraining
+//
+//  Created by Alex Oliveira on 24/10/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
